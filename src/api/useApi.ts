ec053680a9e45859c479c78b11f9cc6f3ff96130
Artem Egorov <artegtor@gmail.com>
2023-05-30 19:34:12 +0400

    add src/api/ : for greenApi

---
+++ src/api/useApi.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const url: string = 'https://api.green-api.com';
+
+const createUrl = (method: string, id: string, token: string): string => [url, `waInstance${id}`, method, token].join('/');
+
+type authState = {
+  stateInstance: null | string;
+}
+
+const useApi = () => {
+
+  const getAccountStatus = async (id: string, token: string) => {
+    const createdUrl = createUrl('getStateInstance', id, token);
+    const { data }: { data: authState } = await axios.get(createdUrl);
+    return data.stateInstance;
+  };
+
+  const sayHello = () => 'hello';
+  return { getAccountStatus, sayHello };
+}
+
+export default useApi;
+
