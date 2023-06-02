c5aeba02bce99f01e02eb956c579396bb7b49b56
Artem Egorov <artegtor@gmail.com>
2023-06-02 13:36:00 +0400

    update src/Components/ChatPage/ : get and send message

--- src/Components/ChatPage/ChatPage.tsx
+++ src/Components/ChatPage/ChatPage.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useRef } from 'react';
 import Aside from "../Aside/Aside";
 import { IoLogoSnapchat } from 'react-icons/io';
 import { FaUserCircle } from 'react-icons/fa';
 import cn from 'classnames';
 import { toast } from 'react-toastify';
+import * as _ from 'lodash';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import './chat.scss';
 import { MdMessage } from 'react-icons/md';
@@ -20,11 +22,41 @@ const formatedMessage = (message: string) => (
 )
 
 const ChatPage = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const dispatch = useAppDispatch();
-  const { sendMessage } = useApi();
-  const { activeChatId, messages } = useAppSelector((state) => state.chatSlice);
+  const { sendMessage, receiveNotification } = useApi();
+  const { activeChatId, messages, chatIds } = useAppSelector((state) => state.chatSlice);
   const currentMessages = activeChatId ? messages[activeChatId] || [] : [];
   const { userData: { id, token } } = useAppSelector((state) => state.userSlice);
+
+  useEffect(() => {
+    inputRef?.current?.focus();
+    const getNewMessages = async () => {
+      try {
+
+        if (id && token) {
+          const response = await receiveNotification(id, token);
+          if (!response) {
+            return;
+          }
+          if (chatIds.includes(response.body.senderData.sender)) {
+            const newMessage = {
+              from: response?.body?.senderData?.sender,
+              text: response?.body?.messageData?.textMessageData?.textMessage,
+            }
+            dispatch(setMessage(newMessage));
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    if (id && token) {
+      const timer = setInterval(() => getNewMessages(), 7000);
+      return () => clearInterval(timer);
+    }
+  }, [activeChatId])
+
   const signUpSchema = yup.object().shape({
     message: yup.string().min(1, 'Не менее 1 символа').required('Обязательное поле')
   });
@@ -37,19 +69,23 @@ const ChatPage = () => {
     onSubmit: async ({ message }) => {
       try {
         if (activeChatId !== null && id !== null && token !== null) {
-          sendMessage(activeChatId, message, id, token);
+          await sendMessage(activeChatId, message, id, token);
           const formatMessage = formatedMessage(message);
           dispatch(setMessage(formatMessage));
           formik.resetForm();
         }
       } catch (err: any) {
-        console.log(err);
+        console.log('!!!!!!!!!!!!!', err);
         toast.error(err.message);
       }
     },
   });
   const inputClass = () => cn('chat__input', {
     'form__input-error': formik.errors.message && formik.touched.message,
+  });
+  const messageClass = (senderMessage: string) => cn('chat__message', {
+    'chat__message-you': senderMessage === 'you',
+    'chat__message-companion': senderMessage !== 'you',
   });
   return (
     <>
@@ -67,8 +103,8 @@ const ChatPage = () => {
             </div>
             <div className="chat__messages">
               {currentMessages.length > 0 ? currentMessages.map((message) => (
-                <div className="chat__message">
-                  <span><b>{message.from}</b>: {message.text}</span>
+                <div className={messageClass(message.from)} key={_.uniqueId()}>
+                  <span>{message.text}</span>
                 </div>
               )) : null}
             </div>
@@ -82,6 +118,7 @@ const ChatPage = () => {
                   value={formik.values.message}
                   placeholder="Введите сообщение..."
                   className={inputClass()}
+                  ref={inputRef}
                   required
                 />
                 {formik.errors.message && formik.touched.message && <p className="form__error-text">{formik.errors.message}</p>}
