931051d3a314d27ae83e06b1d94382343d201121
Artem Egorov <artegtor@gmail.com>
2023-06-02 13:34:40 +0400

    update useApi.ts : add receiv delete notification

--- src/api/useApi.ts
+++ src/api/useApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const url: string = 'https://api.green-api.com';
 
-const createUrl = (method: string, id: string, token: string): string => [url, `waInstance${id}`, method, token].join('/');
+const createUrl = (method: string, id: string, token: string, receiptId?: string): string => [url, `waInstance${id}`, method, token, receiptId].join('/');
 
 type authState = {
   stateInstance: null | string;
@@ -29,8 +29,32 @@ const useApi = () => {
     return response;
   };
 
-  return { getAccountStatus, sendMessage };
+  const receiveNotification = async (id: string, token: string) => {
+    // GET https://api.green-api.com/waInstance{{idInstance}}/receiveNotification/{{apiTokenInstance}}
+    try {
+      const createdUrl = createUrl('receiveNotification', id, token);
+      const response = await axios.get(createdUrl);
+      if (!(response.data)) {
+        console.log('сработал return', createdUrl);
+        return;
+      }
+      const { receiptId } = response.data;
+      const deleteNot = await deleteNotification(id, token, receiptId);
+      console.log(deleteNot, 'DELETE');
+      return response.data;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  const deleteNotification = async (id: string, token: string, receiptId: string) => {
+    // DELETE https://api.green-api.com/waInstance{{idInstance}}/deleteNotification/{{apiTokenInstance}}/{{receiptId}}
+    const createdUrl = createUrl('deleteNotification', id, token, receiptId);
+    const response = await axios.delete(createdUrl);
+    return response;
+  }
+
+  return { getAccountStatus, sendMessage, receiveNotification };
 }
 
 export default useApi;
-
